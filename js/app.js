24560758969f004abf1b274e293a618dd40f812c
vidhya <vid.sigsoln@gmail.com>
2015-10-12 17:41:38 +0000

    use side nav to dislay images

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
+/*
+var drawRect = function draw(){
+        var canvas1 = document.getElementById('side-nav-1');
+        if (canvas1.getContext){
+          var ctx1 = canvas1.getContext('2d');
+          ctx1.fillStyle = "rgb(200,0,0)";
+          ctx1.fillRect (10, 10, 55, 50);
+        }
+        var canvas2 = document.getElementById('side-nav-2');
+        if (canvas2.getContext){
+          var ctx2 = canvas2.getContext('2d');
+          ctx2.fillStyle = "rgb(0,200,0)";
+          ctx2.fillRect (10, 10, 55, 50);
+        }
+        var canvas3 = document.getElementById('side-nav-3');
+        if (canvas3.getContext){
+          var ctx3 = canvas3.getContext('2d');
+          ctx3.fillStyle = "rgb(0,0,200)";
+          ctx3.fillRect (10, 10, 55, 50);
+        }
+};
+
+
+$(document).ready(drawRect); */
 $(document).foundation();
+
+ 
